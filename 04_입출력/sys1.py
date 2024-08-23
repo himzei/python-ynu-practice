3009c459a0cc78fe435a0bd909d65b9293daee3f
himzei <himzei@gmail.com>
2024-08-23 16:19:58 +0900

    240323 1620

--- 04_입출력/sys1.py
+++ 04_입출력/sys1.py
@@ -4,7 +4,8 @@ args = sys.argv[1:]
 print(args)
 
 
-# result = 0
-# for i in args:
-#     result += int(i) 
-# print(result)
+result = 0
+for i in args:
+    # result += int(i) 
+    result = result + int(i)
+print(result)
